adfdd9d4e7cf23dd017c7eb62d627dd1725a775f
Federico Otero <fedeotero@MacBook-Pro-de-Federico.local>
2023-10-08 20:14:04 -0300

    update feature_engineering.py and predict.py

--- resolucion/src/feature_engineering.py
+++ resolucion/src/feature_engineering.py
@@ -1,61 +1,146 @@
 """
 feature_engineering.py
 
-COMPLETAR DOCSTRING
-
-DESCRIPCIÓN:
-AUTOR:
-FECHA:
+DESCRIPTION: Script that prepares the original dataset to be used in a predictive model
+AUTHORS:
+Grupo AdMII: 
+        - Federico Otero - fede.e.otero@gmail.com, 
+        - Rodrigo Carranza - rodrigocarranza81@gmail.com, 
+        - Agustin Menara - menaragustin@gmail.com
+DATE: 10-08-2023
 """
 
 # Imports
+import pandas as pd
 
 class FeatureEngineeringPipeline(object):
 
-    def __init__(self, input_path, output_path):
-        self.input_path = input_path
+    def __init__(self, input_train_path, input_test_path, output_path):
+        self.input_train_path = input_train_path
+        self.input_test_path = input_test_path
         self.output_path = output_path
 
     def read_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING 
-        
-        :return pandas_df: The desired DataLake table as a DataFrame
+        :description: Reads the original dataset and returns a pandas DataFrame
+        :parameters: None      
+        :return data: The desired DataLake table as a DataFrame
         :rtype: pd.DataFrame
         """
-            
-        # COMPLETAR CON CÓDIGO
-        
-        return pandas_df
 
+        # It checks if the original dataset is already split into train and test
+        if self.input_test_path:                
+            data_train = pd.read_csv(self.input_train_path)
+            data_test = pd.read_csv(self.input_test_path)
+            data_train['Set'] = 'train'
+            data_test['Set'] = 'test'
+            data = pd.concat([data_train, data_test], ignore_index=True, sort=False)
+        else:
+            data = pd.read_csv(self.input_train_path)            
+
+        return data
+    
+    def replace_null_values(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        :description: Replace null values in the features of the DataFrame
+        :parameters: 
+        df: Pandas DataFrame 
+        :return df:
+        :rtype: pd.DataFrame
+        """
+        products_ids = list(df[df['Item_Weight'].isnull()]['Item_Identifier'].unique())
+        for _id in products_ids:
+            _mode = (df[df['Item_Identifier'] == _id][['Item_Weight']]).mode().iloc[0,0]
+            df.loc[df['Item_Identifier'] == _id, 'Item_Weight'] = _mode
+
+        outlets = list(df[df['Outlet_Size'].isnull()]['Outlet_Identifier'].unique())
+        for outlet in outlets:
+            df.loc[df['Outlet_Identifier'] == outlet, 'Outlet_Size'] =  'Small'
+
+        return df
+    
+    def transform_features(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        :description: Apply transformations to some features
+        :parameters: 
+        df: Pandas DataFrame 
+        :return df:
+        :rtype: pd.DataFrame
+        """
+
+        df['Outlet_Type'] = df['Outlet_Type'].replace(
+            {'Supermarket Type1': 'Supermarket_Type1', 'Supermarket Type2': 'Supermarket_Type2',
+             'Supermarket Type3': 'Supermarket_Type3', 'Grocery Store': 'Grocery_Store'}
+        )
+
+        df['Item_MRP'] = pd.qcut(df['Item_MRP'], 4, labels = [1, 2, 3, 4])
+        df['Item_MRP'] = df['Item_MRP'].astype(int)
+        return df
+
+
+    def encode_features(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        :description: Encode categorial variables of the DataFrame
+        :parameters: 
+        df: Pandas DataFrame 
+        :return df:
+        :rtype: pd.DataFrame
+        """
+
+        df['Outlet_Size'] = df['Outlet_Size'].replace({'High': 2, 'Medium': 1, 'Small': 0})
+        df['Outlet_Location_Type'] = df['Outlet_Location_Type'].replace({'Tier 1': 2, 'Tier 2': 1, 'Tier 3': 0})
+        df = pd.get_dummies(df,columns=['Outlet_Type'],dtype=int)
+        return df
     
     def data_transformation(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING
-        
+        :description: Apply transformations to the original dataset in order to have appropiate data to feed the model
+        :parameters: 
+        df: Pandas DataFrame 
+        :return df_transformed:
+        :rtype: pd.DataFrame
         """
-        
-        # COMPLETAR CON CÓDIGO
+
+        # Replace null values
+        df = self.replace_null_values(df)
+
+        # Features transformations
+        df = self.transform_features(df)
+
+        # Encoding categorial features
+        df = self.encode_features(df)
+
+        # Delete columns
+        df_transformed = df.drop(columns=['Item_Type', 'Item_Fat_Content']).copy()    
         
         return df_transformed
 
-    def write_prepared_data(self, transformed_dataframe: pd.DataFrame) -> None:
+    def write_prepared_data(self, df_transformed: pd.DataFrame) -> None:
         """
-        COMPLETAR DOCSTRING
-        
+        :description: Write the data recently transformed to the pre_processed folder in csv format
+        :parameters: 
+        df_transformed: Pandas DataFrame
         """
-        
-        # COMPLETAR CON CÓDIGO
-        
-        return None
+
+        # Reorder columns
+        df_to_write = df_transformed[['Item_Identifier','Item_Weight', 'Item_Visibility', 'Item_MRP',
+            'Outlet_Identifier','Outlet_Establishment_Year','Outlet_Size',
+            'Outlet_Location_Type', 'Outlet_Type_Grocery_Store', 'Outlet_Type_Supermarket_Type1',
+            'Outlet_Type_Supermarket_Type2', 'Outlet_Type_Supermarket_Type3', 'Item_Outlet_Sales',
+            'Set']]
+
+        df_to_write.to_csv(self.output_path, sep=',', index=False)
 
     def run(self):
+        """
+        :description: Runs the three steps defined above: read, transform and write.
+        """
     
         df = self.read_data()
         df_transformed = self.data_transformation(df)
         self.write_prepared_data(df_transformed)
-
   
 if __name__ == "__main__":
-    FeatureEngineeringPipeline(input_path = 'Ruta/De/Donde/Voy/A/Leer/Mis/Datos',
-                               output_path = 'Ruta/Donde/Voy/A/Escribir/Mi/Archivo').run()
+    FeatureEngineeringPipeline(input_train_path = '../data/raw/Train_BigMart.csv',
+                               input_test_path = '../data/raw/Test_BigMart.csv',
+                               output_path = '../data/pre_processed/Preprocessed_BigMart.csv').run()
--- resolucion/src/predict.py
+++ resolucion/src/predict.py
@@ -1,14 +1,27 @@
 """
 predict.py
 
-COMPLETAR DOCSTRING
-
 DESCRIPCIÓN:
-AUTOR:
-FECHA:
+
+Scrip para realizar predicciones a partir de un csv.
+Se requiere pasar un path al archivo con las muestras a predecir,
+otro path donde se guardan las predicciones y el path al modelo
+serializado a utilizar.
+
+AUTOR/ES:
+    Grupo AdMII: 
+        - Federico Otero - fede.e.otero@gmail.com, 
+        - Rodrigo Carranza - rodrigocarranza81@gmail.com, 
+        - Agustin Menara - menaragustin@gmail.com
+
+FECHA: 07/10/2023
 """
 
 # Imports
+import pickle
+import pandas as pd
+from feature_engineering import FeatureEngineeringPipeline
+
 
 class MakePredictionPipeline(object):
     
@@ -20,51 +33,95 @@ class MakePredictionPipeline(object):
                 
     def load_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING
+        Metodo para leer la data a predecir a partir del input_path.
+        Se aplica feature_engineering antes de devolver la data
+
+        Parametros:
+            - None
+
+        Return:
+            - data (pd.DataFrame): dataframe con los datos a predecir
         """
+
+        extension = '.' + self.input_path.split('.')[-1]
+        out_fe_path = self.input_path.replace(extension,f'_fe{extension}')
+
+        fe_o = FeatureEngineeringPipeline(input_path = self.input_path,
+                                          output_path = out_fe_path)
+        fe_o.run()
+
+        data = pd.read_csv(out_fe_path)
 
         return data
 
     def load_model(self) -> None:
         """
-        COMPLETAR DOCSTRING
+        Metodo para cargar el modelo como un atributo de la clase.
+
+        Parametros:
+            - None
+
+        Return:
+            - None
         """    
-        self.model = load_model(self.model_path) # Esta función es genérica, utilizar la función correcta de la biblioteca correspondiente
+        
+        # Load from file
+        with open(self.model_path, 'rb') as file:
+            self.model = pickle.load(file) 
         
         return None
 
 
-    def make_predictions(self, data: DataFrame) -> pd.DataFrame:
+    def make_predictions(self, data: pd.DataFrame) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING
+        Metodo para consultar al modelo y realizar las predicciones.
+
+        Parametros:
+            - data (pd.DataFrame): dataframe con las muestras que se quiere predecir
+
+        Return:
+            - new_data (pd.DataFrame): dataframe con la columna id de las muestras
+                                       a predecir y su prediccion
         """
    
-        new_data = self.model.predict(data)
+        predictions = self.model.predict(data)
+        new_data = pd.DataFrame(predictions,columns=['pred_Sales'])
+        
 
         return new_data
 
 
-    def write_predictions(self, predicted_data: DataFrame) -> None:
+    def write_predictions(self, predicted_data: pd.DataFrame) -> None:
         """
-        COMPLETAR DOCSTRING
+        Metodo para guardar las predicciones calculadas.
+
+        Parametros:
+            - predicted_data (pd.DataFrame): dataframe con id y prediccion calculada
+
+        Return:
+            - None
         """
+
+        predicted_data.to_csv(self.output_path)
 
         return None
 
 
     def run(self):
-
+        print('Getting data')
         data = self.load_data()
+        print('Loading model')
         self.load_model()
+        print('Make predictions')
         df_preds = self.make_predictions(data)
+        print('Write preds')
         self.write_predictions(df_preds)
+        print('DONE')
 
 
 if __name__ == "__main__":
-    
-    spark = Spark()
-    
-    pipeline = MakePredictionPipeline(input_path = 'Ruta/De/Donde/Voy/A/Leer/Mis/Datos',
-                                      output_path = 'Ruta/Donde/Voy/A/Escribir/Mis/Datos',
-                                      model_path = 'Ruta/De/Donde/Voy/A/Leer/Mi/Modelo')
+        
+    pipeline = MakePredictionPipeline(input_path = '../data/data_test_sample.csv',
+                                      output_path = '../data/Test_BigMart_predicciones.csv',
+                                      model_path = '../model/pickle_model.pkl')
     pipeline.run()  
